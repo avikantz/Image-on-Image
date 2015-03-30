24cd74b4c82a101c2b02aa29ec8da2663d1989fb
avikantz <avikantsainidbz@gmail.com>
2015-03-30 18:52:13 +0530

    Initial Commit

---
+++ LogoOnImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogoOnImageDemo
+//
+//  Created by Avikant Saini on 3/30/15.
+//  Copyright (c) 2015 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogoOnImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogoOnImageDemo
+//
+//  Created by Avikant Saini on 3/30/15.
+//  Copyright (c) 2015 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
